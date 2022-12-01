fa0d3a9147dcb17daacd00e3e2ab35ba2a4f0f06
testac00000 <82754714+testac00000@users.noreply.github.com>
2022-12-01 17:31:23 +0530

    updated solution
    created the palindrome function

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+const len = str.length;
+  for(let i=0;i < len/2; i++){
+  if (str[i] !== str[len - 1- i]){
+  return false;
+  }
+  }
+  return true;
 }
 module.exports = palindrome
